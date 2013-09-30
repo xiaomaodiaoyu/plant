06fcc3603ed7c6a60885bf725b5217f970a39b26
zhj <zhj.via@gmail.com>
2013-09-30 02:48:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plant::Application.config.secret_key_base = '6d6ddb855bbeab993eb5539e4662f68145de29f03bb75ec8a0a9cba34cf7ea7e6b57adeea3ebb62420c20860dfe9d30acddfc65ec3ddb2f36e526abf6005bab2'
